acf97a20ed8e2e7e8a631a112603ac05c70d7d6f
Eric Pelletier <2495211@edu.vaniercollege.qc.ca>
2025-01-06 10:30:44 -0500

    Lab1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World who are you?");
+            var name = Console.ReadLine();
+            String Namex2 = name + name;
+            string names = Namex2;
+
+            Console.WriteLine(name + ".."+ Namex2 + ".." +names);
+        }
+    }
+}
